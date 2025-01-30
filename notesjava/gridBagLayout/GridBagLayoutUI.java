f119d444b139d8eb4ceb4d0a26f5d1bdebcfb2d8
Citizshrestha <citizshresthaa@gmail.com>
2025-01-30 17:09:09 +0545

    GridBagLayout Added

--- notesjava/gridBagLayout/GridBagLayoutUI.java
+++ notesjava/gridBagLayout/GridBagLayoutUI.java
@@ -1,62 +1,69 @@
 package notesjava.gridBagLayout;
 
 
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+// import java.awt.Button;
+// import java.awt.Frame;
+// import java.awt.GridBagConstraints;
+// import java.awt.GridBagLayout;
+// import java.awt.event.WindowAdapter;
+// import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GridBagLayoutUI extends Frame {
-    public static void main(String[] args) {
-        new GridBagLayoutUI();
-    }
-    GridBagLayoutUI() {
-        
-        GridBagLayout layout = new GridBagLayout();
-        setSize(300, 300);
-        setLayout(layout);
-        setTitle("Grid Bag Layout Demo");
-        setVisible(true);
-        
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        add(new Button("Button 1"), constraints);
-        
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        add(new Button("Button 2"), constraints);
-        
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.ipady = 15;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        add(new Button("Button 3"), constraints);
-        
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        add(new Button("Button 4"), constraints);
-        
-        constraints.gridwidth = 2;
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        add(new Button("Button 5"), constraints);
-     
-        onClose();
-    }
-    
-    public void onClose() {
-        
-        // To close the window after clicking X button.
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
-    }
-    
+import notesjava.gridLayout.GridLayoutUI;
+
+public class GridBagLayoutUI extends Frame{
+  public static void main(String[] args) {
+    new GridBagLayoutUI();
+  }
+  public GridBagLayoutUI(){
+    GridBagLayout gridbagLayout = new GridBagLayout();
+    setLayout(gridbagLayout);
+    setSize(300,300);
+    setTitle("GridbagLaYOUT Demo");
+    setVisible(true);
+
+    Label user = new Label("Username");
+    TextField userTxt = new TextField(10);
+    Button loginBtn = new Button("Login");
+
+    Label pwd = new Label("Password");
+    TextField pwdText = new TextField(10);
+
+  
+
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.gridx = 0;
+    constraints.gridy  = 0;
+    add(user,constraints);
+
+    constraints.gridx = 1;
+    constraints.gridy = 0;
+    add(userTxt,constraints);
+
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.gridx = 0;
+    constraints.gridy = 1;
+    add(pwd,constraints);
+
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    add(pwdText,constraints);
+
+    constraints.gridwidth =2;
+    constraints.gridx  = 0;
+    constraints.gridy  = 2;
+    add(loginBtn,constraints);
+
+    onClose();
+  }
+  public void onClose(){
+    addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e){
+        dispose();
+      }
+    });
+  }
 }
