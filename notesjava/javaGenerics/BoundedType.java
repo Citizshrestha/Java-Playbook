6f66591bc3ffca36ffda27d22ac0d34e6ee6f747
Citizshrestha <citizshresthaa@gmail.com>
2025-02-07 21:31:47 +0545

    Bounded Type extending Class

---
+++ notesjava/javaGenerics/BoundedType.java
@@ -0,0 +1,40 @@
+package notesjava.javaGenerics;
+
+class A{
+  public void display(){
+    System.out.println("Super class A");
+  }
+}
+class B extends A{
+  public void display(){
+    System.out.println(" class B");
+  }
+}
+class C extends A{
+  public void display(){
+    System.out.println(" class C");
+  }
+}
+
+   class Box<T extends A>{
+    T obj;
+    public Box(T obj){
+      this.obj =  obj;
+    }
+
+    public void doTest(){
+      this.obj.display();
+    }
+  }
+public class BoundedType {
+  public static void main(String[] args) {
+    Box<C> BoxC =  new Box<C>(new C());
+    BoxC.doTest();
+
+    Box<B> BoxB = new Box<B>(new B());
+    BoxB.doTest();
+
+    Box<A> BoxA = new Box<A>( new A());
+    BoxA.doTest();
+  }
+}
